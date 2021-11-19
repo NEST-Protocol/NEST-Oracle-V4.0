857da989fc132634b945fc7dc4f8f8bfb2f26b61
chenf <baiynui@hotmail.com>
2021-11-19 10:20:16 +0800

    Change NEST_REDUCTION_SPAN to 10000000

--- contracts/NestOpenMining.sol
+++ contracts/NestOpenMining.sol
@@ -1133,10 +1133,10 @@ contract NestOpenMining is NestBase, INestOpenMining {
     }
 
     // Nest ore drawing attenuation interval. 2400000 blocks, about one year
-    uint constant NEST_REDUCTION_SPAN = 2400000;
+    uint constant NEST_REDUCTION_SPAN = 10000000;
     // The decay limit of nest ore drawing becomes stable after exceeding this interval. 
     // 24 million blocks, about 10 years
-    uint constant NEST_REDUCTION_LIMIT = 24000000; //NEST_REDUCTION_SPAN * 10;
+    uint constant NEST_REDUCTION_LIMIT = 100000000; //NEST_REDUCTION_SPAN * 10;
     // Attenuation gradient array, each attenuation step value occupies 16 bits. The attenuation value is an integer
     uint constant NEST_REDUCTION_STEPS = 0x280035004300530068008300A300CC010001400190;
         // 0
