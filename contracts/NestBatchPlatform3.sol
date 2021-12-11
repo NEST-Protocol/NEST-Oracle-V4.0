3e7b51697933cb7c059ea52f4a5c8a81a4316a83
chenf <baiynui@hotmail.com>
2021-12-11 11:12:48 +0800

    Test query price

--- contracts/NestBatchPlatform3.sol
+++ contracts/NestBatchPlatform3.sol
@@ -11,135 +11,135 @@ import "./NestBatchMining.sol";
 
 // 支持pairIndex数组，可以一次性查询多个价格
 /// @dev This contract implemented the mining logic of nest
-contract NestBatchPlatform2 is NestBatchMining, INestBatchPriceView {
+contract NestBatchPlatform3 is NestBatchMining {
 
     // TODO: 支持pairIndex数组，可以一次性查询多个价格
 
-    /// @dev Get the latest trigger price
-    /// @param channelId 报价通道编号
-    /// @param pairIndex 报价对编号
-    /// @return blockNumber The block number of price
-    /// @return price The token price. (1eth equivalent to (price) token)
-    function triggeredPrice(uint channelId, uint pairIndex) external view override noContract returns (uint blockNumber, uint price) {
-        return _triggeredPrice(_channels[channelId].pairs[pairIndex]);
-    }
-
-    /// @dev Get the full information of latest trigger price
-    /// @param channelId 报价通道编号
-    /// @param pairIndex 报价对编号
-    /// @return blockNumber The block number of price
-    /// @return price The token price. (1eth equivalent to (price) token)
-    /// @return avgPrice Average price
-    /// @return sigmaSQ The square of the volatility (18 decimal places). The current implementation assumes that 
-    ///         the volatility cannot exceed 1. Correspondingly, when the return value is equal to 999999999999996447,
-    ///         it means that the volatility has exceeded the range that can be expressed
-    function triggeredPriceInfo(uint channelId, uint pairIndex) external view override noContract returns (
-        uint blockNumber,
-        uint price,
-        uint avgPrice,
-        uint sigmaSQ
-    ) {
-        return _triggeredPriceInfo(_channels[channelId].pairs[pairIndex]);
-    }
-
-    /// @dev Find the price at block number
-    /// @param channelId 报价通道编号
-    /// @param pairIndex 报价对编号
-    /// @param height Destination block number
-    /// @return blockNumber The block number of price
-    /// @return price The token price. (1eth equivalent to (price) token)
-    function findPrice(
-        uint channelId,
-        uint pairIndex,
-        uint height
-    ) external view override noContract returns (uint blockNumber, uint price) {
-        return _findPrice(_channels[channelId].pairs[pairIndex], height);
-    }
-
-    /// @dev Get the latest effective price
-    /// @param channelId 报价通道编号
-    /// @param pairIndex 报价对编号
-    /// @return blockNumber The block number of price
-    /// @return price The token price. (1eth equivalent to (price) token)
-    function latestPrice(uint channelId, uint pairIndex) external view override noContract returns (uint blockNumber, uint price) {
-        return _latestPrice(_channels[channelId].pairs[pairIndex]);
-    }
-
-    /// @dev Get the last (num) effective price
-    /// @param channelId 报价通道编号
-    /// @param pairIndex 报价对编号
-    /// @param count The number of prices that want to return
-    /// @return An array which length is num * 2, each two element expresses one price like blockNumber｜price
-    function lastPriceList(uint channelId, uint pairIndex, uint count) external view override noContract returns (uint[] memory) {
-        return _lastPriceList(_channels[channelId].pairs[pairIndex], count);
-    } 
-
-    // /// @dev Returns the results of latestPrice() and triggeredPriceInfo()
+    // /// @dev Get the latest trigger price
     // /// @param channelId 报价通道编号
     // /// @param pairIndex 报价对编号
-    // /// @return latestPriceBlockNumber The block number of latest price
-    // /// @return latestPriceValue The token latest price. (1eth equivalent to (price) token)
+    // /// @return blockNumber The block number of price
+    // /// @return price The token price. (1eth equivalent to (price) token)
+    // function triggeredPrice(uint channelId, uint pairIndex) external view override noContract returns (uint blockNumber, uint price) {
+    //     return _triggeredPrice(_channels[channelId].pairs[pairIndex]);
+    // }
+
+    // /// @dev Get the full information of latest trigger price
+    // /// @param channelId 报价通道编号
+    // /// @param pairIndex 报价对编号
+    // /// @return blockNumber The block number of price
+    // /// @return price The token price. (1eth equivalent to (price) token)
+    // /// @return avgPrice Average price
+    // /// @return sigmaSQ The square of the volatility (18 decimal places). The current implementation assumes that 
+    // ///         the volatility cannot exceed 1. Correspondingly, when the return value is equal to 999999999999996447,
+    // ///         it means that the volatility has exceeded the range that can be expressed
+    // function triggeredPriceInfo(uint channelId, uint pairIndex) external view override noContract returns (
+    //     uint blockNumber,
+    //     uint price,
+    //     uint avgPrice,
+    //     uint sigmaSQ
+    // ) {
+    //     return _triggeredPriceInfo(_channels[channelId].pairs[pairIndex]);
+    // }
+
+    // /// @dev Find the price at block number
+    // /// @param channelId 报价通道编号
+    // /// @param pairIndex 报价对编号
+    // /// @param height Destination block number
+    // /// @return blockNumber The block number of price
+    // /// @return price The token price. (1eth equivalent to (price) token)
+    // function findPrice(
+    //     uint channelId,
+    //     uint pairIndex,
+    //     uint height
+    // ) external view override noContract returns (uint blockNumber, uint price) {
+    //     return _findPrice(_channels[channelId].pairs[pairIndex], height);
+    // }
+
+    // /// @dev Get the latest effective price
+    // /// @param channelId 报价通道编号
+    // /// @param pairIndex 报价对编号
+    // /// @return blockNumber The block number of price
+    // /// @return price The token price. (1eth equivalent to (price) token)
+    // function latestPrice(uint channelId, uint pairIndex) external view override noContract returns (uint blockNumber, uint price) {
+    //     return _latestPrice(_channels[channelId].pairs[pairIndex]);
+    // }
+
+    // /// @dev Get the last (num) effective price
+    // /// @param channelId 报价通道编号
+    // /// @param pairIndex 报价对编号
+    // /// @param count The number of prices that want to return
+    // /// @return An array which length is num * 2, each two element expresses one price like blockNumber｜price
+    // function lastPriceList(uint channelId, uint pairIndex, uint count) external view override noContract returns (uint[] memory) {
+    //     return _lastPriceList(_channels[channelId].pairs[pairIndex], count);
+    // } 
+
+    // // /// @dev Returns the results of latestPrice() and triggeredPriceInfo()
+    // // /// @param channelId 报价通道编号
+    // // /// @param pairIndex 报价对编号
+    // // /// @return latestPriceBlockNumber The block number of latest price
+    // // /// @return latestPriceValue The token latest price. (1eth equivalent to (price) token)
+    // // /// @return triggeredPriceBlockNumber The block number of triggered price
+    // // /// @return triggeredPriceValue The token triggered price. (1eth equivalent to (price) token)
+    // // /// @return triggeredAvgPrice Average price
+    // // /// @return triggeredSigmaSQ The square of the volatility (18 decimal places). The current implementation 
+    // // /// assumes that the volatility cannot exceed 1. Correspondingly, when the return value is equal to 
+    // // /// 999999999999996447, it means that the volatility has exceeded the range that can be expressed
+    // // function latestPriceAndTriggeredPriceInfo(uint channelId, uint pairIndex) external view noContract
+    // // returns (
+    // //     uint latestPriceBlockNumber,
+    // //     uint latestPriceValue,
+    // //     uint triggeredPriceBlockNumber,
+    // //     uint triggeredPriceValue,
+    // //     uint triggeredAvgPrice,
+    // //     uint triggeredSigmaSQ
+    // // ) {
+    // //     return _latestPriceAndTriggeredPriceInfo(_channels[channelId].pairs[pairIndex]);
+    // // }
+
+    // /// @dev Returns lastPriceList and triggered price info
+    // /// @param channelId 报价通道编号
+    // /// @param pairIndex 报价对编号
+    // /// @param count The number of prices that want to return
+    // /// @return prices An array which length is num * 2, each two element expresses one price like blockNumber｜price
     // /// @return triggeredPriceBlockNumber The block number of triggered price
     // /// @return triggeredPriceValue The token triggered price. (1eth equivalent to (price) token)
     // /// @return triggeredAvgPrice Average price
     // /// @return triggeredSigmaSQ The square of the volatility (18 decimal places). The current implementation 
     // /// assumes that the volatility cannot exceed 1. Correspondingly, when the return value is equal to 
     // /// 999999999999996447, it means that the volatility has exceeded the range that can be expressed
-    // function latestPriceAndTriggeredPriceInfo(uint channelId, uint pairIndex) external view noContract
+    // function lastPriceListAndTriggeredPriceInfo(uint channelId, uint pairIndex, uint count) external view override noContract
     // returns (
-    //     uint latestPriceBlockNumber,
-    //     uint latestPriceValue,
+    //     uint[] memory prices,
     //     uint triggeredPriceBlockNumber,
     //     uint triggeredPriceValue,
     //     uint triggeredAvgPrice,
     //     uint triggeredSigmaSQ
     // ) {
-    //     return _latestPriceAndTriggeredPriceInfo(_channels[channelId].pairs[pairIndex]);
+    //     //return _lastPriceListAndTriggeredPriceInfo(_channels[channelId].pairs[pairIndex], count);
+    //     PricePair storage pair = _channels[channelId].pairs[pairIndex];
+    //     prices = _lastPriceList(pair, count);
+    //     (
+    //         triggeredPriceBlockNumber, 
+    //         triggeredPriceValue, 
+    //         triggeredAvgPrice, 
+    //         triggeredSigmaSQ
+    //     ) = _triggeredPriceInfo(pair);
     // }
 
-    /// @dev Returns lastPriceList and triggered price info
-    /// @param channelId 报价通道编号
-    /// @param pairIndex 报价对编号
-    /// @param count The number of prices that want to return
-    /// @return prices An array which length is num * 2, each two element expresses one price like blockNumber｜price
-    /// @return triggeredPriceBlockNumber The block number of triggered price
-    /// @return triggeredPriceValue The token triggered price. (1eth equivalent to (price) token)
-    /// @return triggeredAvgPrice Average price
-    /// @return triggeredSigmaSQ The square of the volatility (18 decimal places). The current implementation 
-    /// assumes that the volatility cannot exceed 1. Correspondingly, when the return value is equal to 
-    /// 999999999999996447, it means that the volatility has exceeded the range that can be expressed
-    function lastPriceListAndTriggeredPriceInfo(uint channelId, uint pairIndex, uint count) external view override noContract
-    returns (
-        uint[] memory prices,
-        uint triggeredPriceBlockNumber,
-        uint triggeredPriceValue,
-        uint triggeredAvgPrice,
-        uint triggeredSigmaSQ
-    ) {
-        //return _lastPriceListAndTriggeredPriceInfo(_channels[channelId].pairs[pairIndex], count);
-        PricePair storage pair = _channels[channelId].pairs[pairIndex];
-        prices = _lastPriceList(pair, count);
-        (
-            triggeredPriceBlockNumber, 
-            triggeredPriceValue, 
-            triggeredAvgPrice, 
-            triggeredSigmaSQ
-        ) = _triggeredPriceInfo(pair);
-    }
-
     // Payment of transfer fee
-    function _pay(uint channelId, address payback) private returns (PriceChannel storage channel) {
+    function _pay(uint channelId, address payback) private view returns (PriceChannel storage channel) {
 
         channel = _channels[channelId];
-        uint fee = uint(channel.singleFee) * DIMI_ETHER;
-        if (msg.value > fee) {
-            //payable(payback).transfer(msg.value - fee);
-            TransferHelper.safeTransferETH(payback, msg.value - fee);
-        } else {
-            require(msg.value == fee, "NOP:!fee");
-        }
+        // uint fee = uint(channel.singleFee) * DIMI_ETHER;
+        // if (msg.value > fee) {
+        //     //payable(payback).transfer(msg.value - fee);
+        //     TransferHelper.safeTransferETH(payback, msg.value - fee);
+        // } else {
+        //     require(msg.value == fee, "NOP:!fee");
+        // }
 
-        channel.rewards += fee;
+        // channel.rewards += fee;
     }
 
     /// @dev Get the latest trigger price
@@ -151,7 +151,7 @@ contract NestBatchPlatform2 is NestBatchMining, INestBatchPriceView {
         uint channelId,
         uint[] calldata pairIndices, 
         address payback
-    ) external payable returns (uint[] memory prices) {
+    ) external view returns (uint[] memory prices) {
         PriceChannel storage channel = _pay(channelId, payback);
 
         uint n = pairIndices.length << 1;
@@ -171,7 +171,7 @@ contract NestBatchPlatform2 is NestBatchMining, INestBatchPriceView {
         uint channelId, 
         uint[] calldata pairIndices,
         address payback
-    ) external payable returns (
+    ) external view returns (
         uint[] memory prices
     ) {
         PriceChannel storage channel = _pay(channelId, payback);
@@ -195,7 +195,7 @@ contract NestBatchPlatform2 is NestBatchMining, INestBatchPriceView {
         uint[] calldata pairIndices, 
         uint height, 
         address payback
-    ) external payable returns (uint[] memory prices) {
+    ) external view returns (uint[] memory prices) {
         PriceChannel storage channel = _pay(channelId, payback);
 
         uint n = pairIndices.length << 1;
@@ -215,7 +215,7 @@ contract NestBatchPlatform2 is NestBatchMining, INestBatchPriceView {
         uint channelId, 
         uint[] calldata pairIndices, 
         address payback
-    ) external payable returns (uint[] memory prices) {
+    ) external view returns (uint[] memory prices) {
         PriceChannel storage channel = _pay(channelId, payback);
 
         uint n = pairIndices.length << 1;
@@ -237,7 +237,7 @@ contract NestBatchPlatform2 is NestBatchMining, INestBatchPriceView {
         uint[] calldata pairIndices, 
         uint count, 
         address payback
-    ) external payable returns (uint[] memory prices) {
+    ) external view returns (uint[] memory prices) {
         PriceChannel storage channel = _pay(channelId, payback);
 
         uint row = count << 1;
@@ -290,7 +290,7 @@ contract NestBatchPlatform2 is NestBatchMining, INestBatchPriceView {
         uint[] calldata pairIndices,
         uint count, 
         address payback
-    ) external payable returns (uint[] memory prices) {
+    ) external view returns (uint[] memory prices) {
         PriceChannel storage channel = _pay(channelId, payback);
 
         uint row = (count << 1) + 4;
