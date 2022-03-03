76e0f2c710575f2bd61307a3f9a843e8d1cc067a
chenf <baiynui@hotmail.com>
2022-03-03 16:14:45 +0800

    Increase vault

--- test/deploy.js
+++ test/deploy.js
@@ -15,9 +15,13 @@ describe('NestOpenMining', function() {
         
         console.log('ok');
         const NestBatchPlatform2New = await ethers.getContractFactory('NestBatchPlatform2New');
-        const nestBatchPlatform2New2 = await NestBatchPlatform2New.deploy();
+        const nestBatchPlatform2New2 = await NestBatchPlatform2New.attach('0xF2f9E62f52389EF223f5Fa8b9926e95386935277');
         console.log('nestBatchPlatform2New2: ' + nestBatchPlatform2New2.address);
 
+        let ci = await nestBatchPlatform2New2.getChannelInfo(0);
+        console.log(ci);
+
+        //await nestBatchPlatform2New2.increase(0, 10000000000000000000000000n);
         // await nestBatchPlatform2New.open(
         //     pusd.address,
         //     toBigInt(2000),
