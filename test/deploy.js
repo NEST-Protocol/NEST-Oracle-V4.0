0f2401eba94bb9a9eac543d42ccf88656d95c627
chenf <baiynui@hotmail.com>
2022-05-06 09:59:44 +0800

    Prepare for update

--- test/deploy.js
+++ test/deploy.js
@@ -16,28 +16,6 @@ describe('NestOpenMining', function() {
         console.log('ok');
 
         const NestBatchPlatform2 = await ethers.getContractFactory('NestBatchPlatform2');
-
-        if (false) {
-            // 2022-05-08 10:00 Deploy and update NNIncome
-            const NNIncome = await ethers.getContractFactory('NNIncome');
-            const newNNIncome = await NNIncome.deploy();
-            console.log('newNNIncome: ' + newNNIncome.address);
-        }
-
-        if (false) {
-            // 2022-05-08 10:00 Deploy and update NestBatchMining
-            const newNestBatchPlatform2 = await NestBatchPlatform2.deploy();
-            console.log('newNestBatchPlatform2: ' + newNestBatchPlatform2.address);
-        }
-
-        if (false) {
-            // 2022-05-08 10:00 Add eth&nest to channel 0
-            const nestBatchPlatform = await NestBatchPlatform2.attach('xxx');
-            await nestBatchPlatform.addPair(0, '0x0000000000000000000000000000000000000000');
-            await nestBatchPlatform.addPair(0, nest.address);
-        }
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         const toPi = function(p) {
             return {
                 target: p.target.toString(),
@@ -75,10 +53,53 @@ describe('NestOpenMining', function() {
             };
         }
 
+        if (false) {
+            // 2022-05-08 10:00 Deploy and update NNIncome
+            // 1. Deploy contract
+            const NNIncome = await ethers.getContractFactory('NNIncome');
+            const newNNIncome = await NNIncome.deploy();
+            console.log('newNNIncome: ' + newNNIncome.address);
+
+            // 2. Verify contract code
+            // 3. Update implementation
+            // Proxy: 0x95557DE67444B556FE6ff8D7939316DA0Aa340B2
+            // ProxyAdmin: 0x7DBe94A4D6530F411A1E7337c7eb84185c4396e6
+
+            // 4. Check view methods, eg. earned
+        }
+
+        if (false) {
+            // 2022-05-08 10:30 Deploy and update NestBatchMining
+            // 1. Deploy contract
+            const newNestBatchPlatform2 = await NestBatchPlatform2.deploy();
+            console.log('newNestBatchPlatform2: ' + newNestBatchPlatform2.address);
+
+            // 2. Verify contract code
+            // 3. Update implementation
+            // Proxy: 0xE544cF993C7d477C7ef8E91D28aCA250D135aa03
+            // ProxyAdmin: 0x7DBe94A4D6530F411A1E7337c7eb84185c4396e6
+
+            // 4. Check view methods. eg. earned
+        }
+
+        if (false) {
+            // 2022-05-08 10:00 Add eth&nest to channel 0
+
+            const nestBatchPlatform = await NestBatchPlatform2.attach('0xE544cF993C7d477C7ef8E91D28aCA250D135aa03');
+            // 1. Add eth pair
+            await nestBatchPlatform.addPair(0, '0x0000000000000000000000000000000000000000');
+            // 2. Add nest pair
+            await nestBatchPlatform.addPair(0, nest.address);
+            // 3. Check channel info
+            console.log(toCi(nestBatchPlatform.getChannelInfo(0)));
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
         // 2022-05-08 10:00 Adjust nest reward speed
 
         // ETH
-        if (true) {
+        if (false) {
             const nestBatchPlatform = await NestBatchPlatform2.attach('0xE544cF993C7d477C7ef8E91D28aCA250D135aa03');
             const c = await nestBatchPlatform.getChannelInfo(0);
             const ci = toCi(c);
@@ -88,6 +109,11 @@ describe('NestOpenMining', function() {
             ci.rewardPerBlock = '24000000000000000000';
             ci.singleFee = '0';
             console.log(toChannelConfig(ci));
+
+            // Update
+            // await nestBatchPlatform.modify(ci);
+
+            // Check
         }
 
         // BSC
@@ -101,6 +127,11 @@ describe('NestOpenMining', function() {
             ci.rewardPerBlock = '1000000000000000000';
             ci.singleFee = '0';
             console.log(toChannelConfig(ci));
+
+            // Update
+            // await nestBatchPlatform.modify(ci);
+
+            // Check
         }
 
         // Polygon
@@ -114,6 +145,11 @@ describe('NestOpenMining', function() {
             ci.rewardPerBlock = '300000000000000000';
             ci.singleFee = '0';
             console.log(toChannelConfig(ci));
+
+            // Update
+            // await nestBatchPlatform.modify(ci);
+
+            // Check
         }
 
         // KCC
@@ -127,6 +163,11 @@ describe('NestOpenMining', function() {
             ci.rewardPerBlock = '100000000000000000';
             ci.singleFee = '0';
             console.log(toChannelConfig(ci));
+
+            // Update
+            // await nestBatchPlatform.modify(ci);
+
+            // Check
         }
     });
 });
